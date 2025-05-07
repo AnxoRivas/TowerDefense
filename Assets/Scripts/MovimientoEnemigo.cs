61f5ff97374998cf362928e72e10b501de5de0fb
AnxoRivas <anxorivasferrero@gmail.com>
2025-05-07 21:08:04 +0200

    Implementación de Animaciones.
    Sigue trabajando con los prefabs y animaciones.

--- Assets/Scripts/MovimientoEnemigo.cs
+++ Assets/Scripts/MovimientoEnemigo.cs
@@ -7,6 +7,7 @@ public class MovimientoEnemigo : MonoBehaviour
     public List<Transform> waypoints = new List<Transform>();
     private int targetIndex = 1;
     public float movementSpeed= 4;
+    public float rotationSpeed = 10f; // Velocidad de rotación
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,6 +21,16 @@ public class MovimientoEnemigo : MonoBehaviour
     }
 
     private void Movement() {
+
+        Vector3 direction = (waypoints[targetIndex].position - transform.position).normalized;
+
+        //Rotar hacia la dirección del movimiento
+        if(direction != Vector3.zero) {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        //Moverse hacia el waypoint
         transform.position = Vector3.MoveTowards(transform.position, waypoints[targetIndex].position, movementSpeed * Time.deltaTime);
         var distance = Vector3.Distance(transform.position, waypoints[targetIndex].position);
         
